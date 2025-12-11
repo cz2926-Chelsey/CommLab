c5a8d9128aaaa382587cba6d7a8864619692f4a8
cz2926-Chelsey <cz2926@nyu.edu>
2025-12-11 14:11:24 +0800

    update

---
+++ Final/scriptfood.js
@@ -0,0 +1,35 @@
+let foods = document.querySelectorAll(".food");
+let bgm = document.querySelector("#bgm");
+let bite = document.querySelector("#bite");
+let back = document.querySelector("#backLink");
+let eatCount = 0;
+
+
+foods.forEach(function (food) {
+    bgm.play()
+
+    let x = Math.random() * 70 + 10;
+    let y = Math.random() * 70 + 10;
+
+    food.style.left = x + "vw";
+    food.style.top = y + "vh";
+
+    food.addEventListener("click", function () {
+
+        if (bite) {
+            bite.currentTime = 0;
+            bite.play();
+        }
+        let lastLetter = food.lastElementChild;
+
+        if (lastLetter) {
+            lastLetter.remove();
+        }
+        eatCount = eatCount + 1;
+        if (eatCount >= 13) {
+            back.style.opacity = 1;
+            back.style.pointerEvents = "auto";
+        }
+    });
+
+});
